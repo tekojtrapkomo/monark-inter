80e340f159783a2c9f1482a7f186ec646728095d
tekojtrapkomo <battogtokhportfolio@gmail.com>
2023-08-18 17:54:04 +0800

    init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,12 @@
+<script>
+// @ts-nocheck
+
+</script>
+
+<nav>
+	<img src="/MONARK-LOGO1.svg" alt="Logo">
+
+</nav>
 
 <style>
 	:global(body) {
@@ -5,6 +14,21 @@
 		padding: 0;
 		font-family: sans-serif;
 		overflow: hidden;
+	}
+	nav {
+		width: 100vw;
+		background-color: red;
+		height: 5svh;
+		display: flex;
+		justify-content: center;
+		flex-direction: column;
+		align-items: center;
+		padding: 0 20px;
+		margin: auto;
+	}
+	nav img {
+		max-width: 400px;
+		max-height: max-content;
 	}
 </style>
 <slot />
